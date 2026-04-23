042c0849a8221dc4041d22c8929910178f089c42
diskman88 <diskman88@163.com>
2026-04-23 10:59:26 +0800

    修改映射，降低RGB亮度范围: 	modified:   libraries/DFRobot_Mindplus_MPython/src/rgb/RGB.cpp

--- libraries/DFRobot_Mindplus_MPython/src/rgb/RGB.cpp
+++ libraries/DFRobot_Mindplus_MPython/src/rgb/RGB.cpp
@@ -6,7 +6,7 @@ RGB::RGB(uint8_t pin, uint16_t numPixels) : _ws2812(numPixels, pin, 0, mPython_T
 
 void RGB::begin() {
     _ws2812.begin();
-    _ws2812.setBrightness((uint8_t)map(_brightness, 0, 9, 0, 255));
+    _ws2812.setBrightness((uint8_t)map(_brightness, 0, 9, 0, 64));
 }
 
 void RGB::write(int8_t index, uint8_t r, uint8_t g, uint8_t b) {
@@ -22,7 +22,7 @@ void RGB::brightness(uint8_t b) {
         return;
     }
     _brightness = max(min(b, (uint8_t)9), (uint8_t)0);
-    _ws2812.setBrightness((uint8_t)map(_brightness, 0, 9, 0, 255));
+    _ws2812.setBrightness((uint8_t)map(_brightness, 0, 9, 0, 64));
 }
 
 uint8_t RGB::brightness() {
